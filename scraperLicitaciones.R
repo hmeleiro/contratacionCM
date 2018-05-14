8d372b5eb137acc14ab5b1248200592ec44a44ef
Meleiro <hmeleiros@gmail.com>
2018-05-14 14:45:24 +0200

    Update scraperLicitaciones.R

--- scraperLicitaciones.R
+++ scraperLicitaciones.R
@@ -1,5 +1,7 @@
-setwd("/Users/meleiro/Dropbox/MASTER/DATOS/R/CONTRATOS MENORES")
+## Scraper de licitaciones para 2015-2017
 
+
+setwd("")  #Poner ruta al directorio de trabajo
 
 require(stringr)
 require(httr)
